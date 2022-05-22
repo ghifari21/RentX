37651ac475f5b2709437ae61c46460a470f8845a
Hilman Fauzi Herdiana <kk726627@gmail.com>
2022-05-22 08:50:43 +0700

    update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,40 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('login');
+});
+
+# routing untuk halaman login
+Route::get('/login', function () {
+    return view('login');
+});
+
+# routing untuk halaman register
+Route::get('/register', function () {
+    return view('register');
+});
+
+# routing untuk halaman buyerProfile
+Route::get('/buyerProfile', function () {
+    return view('buyerProfile');
+});
+
+# routing untuk halaman buyerHistory
+Route::get('/buyerHistory', function () {
+    return view('buyerHistory');
+});
+
+# routing untuk halaman buyerVerification
+Route::get('/buyerVerification', function () {
+    return view('buyerVerification');
+});
+
+# routing untuk halaman sellerProfile
+Route::get('/sellerProfile', function () {
+    return view('sellerProfile');
+});
+
+# routing untuk halaman sellerProfile
+Route::get('/sellerHistory', function () {
+    return view('sellerHistory');
 });
