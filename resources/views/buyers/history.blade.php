fc14ac3c95f261a1cc9194fe83eff7f1e41c12bd
Hilman Fauzi Herdiana <kk726627@gmail.com>
2022-05-22 11:16:01 +0700

    add buyers folder & buyer files

---
+++ resources/views/buyers/history.blade.php
@@ -0,0 +1,35 @@
+@extends('buyers.layouts.main')
+
+@section('container')
+    <div class="row">
+        <!-- left side section -->
+        @include('buyers.layouts.sidebar')
+
+        <!-- right side section -->
+        <div class="col p-3">
+            <h1 class="fs-3 fw-bold mb-3">Riwayat Kos</h1>
+            <div class="border border-secondary rounded px-4 py-3" style="overflow-y: auto; height: 61vh">
+                <div class="row border border-secondary rounded p-3 mb-3">
+                    <div class="col">
+                        <div class="row">
+                            <p class="fw-bold mb-0">{{ $propertyName }}</p>
+                            <p class="">{{ $propertyAddress }}</p>
+                        </div>
+                        <div class="row mt-2 invisible">
+                            <p class="mb-0">{{ $propertyPrice }}</p>
+                        </div>
+                    </div>
+                    <div class="col-4 d-flex align-items-end flex-column">
+                        <div class="row">
+                            <p class="">Durasi: {{ $duration }}</p>
+                        </div>
+                        <div class="row invisible"><p>Invisible</p></div>
+                        <div class="row">
+                                <a href="#"><button class="btn btn-danger mb-0">Sewa Lagi</button></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/buyers/layouts/sidebar.blade.php
@@ -0,0 +1,42 @@
+<div class="col-3 p-3">
+    <!-- identity section -->
+    <div class="row mb-3">
+        <div class="col">
+            <img src="https://media.discordapp.net/attachments/758554583185621042/790106370706898985/131926727_2886493588258144_292419452096811329_n.png" class="rounded img-fluid" alt="Avatar">
+        </div>
+        <div class="col">
+            <div class="row mt-1">
+                <p class="fw-bold mb-0">{{ $name }}</p>
+                <p class="">{{ $email }}</p>
+            </div>
+            <div class="row-1">
+                <a href="#"><button class="btn btn-outline-primary">Edit Profile</button></a>
+            </div>
+        </div>
+    </div>
+    <!-- option section -->
+    <div class="row mb-3 border-bottom border-secondary">
+        <div class="col-1">
+            <i class="fa-solid fa-house"></i>
+        </div>
+        <div class="col">
+            <a href="/buyers/profile" class="text-black" style="text-decoration: none;"><p>Kos Saya</p></a>
+        </div>
+    </div>
+    <div class="row mb-3 border-bottom border-secondary">
+        <div class="col-1">
+            <i class="fa-solid fa-clock-rotate-left"></i>
+        </div>
+        <div class="col">
+            <a href="/buyers/history" class="text-black" style="text-decoration: none;"><p>Riwayat Kos</p></a>
+        </div>
+    </div>
+    <div class="row mb-3 border-bottom border-secondary">
+        <div class="col-1">
+            <i class="fa fa-user-check"></i>
+        </div>
+        <div class="col">
+            <a href="/buyers/verification" class="text-black" style="text-decoration: none;"><p>Verifikasi Akun</p></a>
+        </div>
+    </div>
+</div>
