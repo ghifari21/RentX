ab6c25ef54fc46f8e6987e573a9a3f643f7ae3f3
Hilman Fauzi Herdiana <kk726627@gmail.com>
2022-05-22 11:16:46 +0700

    add partials folder & files

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,5 @@
+<div class="footer fixed-bottom p-1 mt-5" style="background-color: #2055CB;">
+    <div class="text-center text-white fs-5">
+        <p>Copyright @ 2022 by RentX</p>    
+    </div>
+</div>   
---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,8 @@
+<navbar class="navbar py-3 px-5 mb-5" style="background-color: #2055CB;">
+    <a href="#" style="text-decoration: none;"><img src="" alt="RentX" class="ms-4"></a>
+    <div class="nav-button me-4">
+        <i class="fa fa-bell me-2 fs-4"></i>
+        <i class="fa fa-envelope me-5 fs-4"></i>
+        <i class="fa fa-user fs-3"></i>
+    </div>
+</navbar>
