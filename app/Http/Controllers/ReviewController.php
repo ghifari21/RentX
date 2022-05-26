845101d5c2ff5d91fbad39caca8e1b2d34c10597
Ghifari Octaverin <ghifariocta21@gmail.com>
2022-05-26 17:27:47 +0700

    fix buyer_id on review controller

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -12,8 +12,9 @@ use App\Models\Review;
 class ReviewController extends Controller
 {
     public function index(Property $property){
-        $count_1 = Order::where([['property_id','=',$property->id],['buyer_id','=',auth()->user()->id],['status','=','accepted']])->count();
-        $count_2 = Review::where([['property_id','=',$property->id],['buyer_id','=',auth()->user()->id]])->count();
+        $buyer = Buyer::firstWhere('user_id', auth()->user()->id);
+        $count_1 = Order::where([['property_id','=',$property->id],['buyer_id','=',$buyer->id],['status','=','accepted']])->count();
+        $count_2 = Review::where([['property_id','=',$property->id],['buyer_id','=',$buyer->id]])->count();
 
         //dd($count_1,$count_2);
         if($count_1>0 && $count_2==0){
@@ -24,20 +25,21 @@ class ReviewController extends Controller
         }else if($count_1==0 || $count_2!=0){
             //error message
             return "error";
-        }        
+        }
     }
 
     public function store(Request $request,Property $property){
         //validate data
+        $buyer = Buyer::firstWhere('user_id', auth()->user()->id);
         $validatedData = $request->validate([
             'comment'=>'required|max:500',
             'rating'=>'required',
         ]);
         //gk perlu order id
         $validatedData['order_id']=1;
-        $validatedData['buyer_id']=auth()->user()->id;
+        $validatedData['buyer_id']=$buyer->id;
         $validatedData['property_id']=$property->id;
-        
+
         $old_total_reviewer=$property->total_reviewer;
         //kali rating dan total_review di tabel property
         $old_rating_property=($property->rating)*$old_total_reviewer;
@@ -50,7 +52,7 @@ class ReviewController extends Controller
         $property->rating=$new_rating_property;
         //save tabel property
         $property->save();
-        
+
         //create
         Review::create($validatedData);
 
