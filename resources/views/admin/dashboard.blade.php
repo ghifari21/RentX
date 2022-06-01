7c834c1d2d9938ebe8e9a5e091096fda4d26fe43
Hilman Fauzi Herdiana <kk726627@gmail.com>
2022-06-02 02:31:03 +0700

    changes on admin dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -6,16 +6,6 @@
         <hr>
 
         <div class="row p-3 mt-2 gap-4">
-            <div class="col border rounded" style="background-color: #ACE98D;">
-                <ul class="nav flex-column mb-auto">
-                    <li>
-                        <a href="/admin/transaksi" class="nav-link text-black fs-5">
-                            <p class="fw-bold">Transaksi</p>
-                            <hr>
-                        </a>
-                    </li>
-                </ul>
-            </div>
             <div class="col border rounded" style="background-color: #F1EE7A;">
                 <ul class="nav flex-column mb-auto">
                     <li>
@@ -41,6 +31,16 @@
                     <li>
                         <a href="/admin/daftarBuyer" class="nav-link text-black fs-5">
                             <p class="fw-bold">Daftar Buyer</p>
+                            <hr>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col border rounded" style="background-color: #ACE98D;">
+                <ul class="nav flex-column mb-auto">
+                    <li>
+                        <a href="/admin/requestUpgrade" class="nav-link text-black fs-5">
+                            <p class="fw-bold">Request Jadi Seller</p>
                             <hr>
                         </a>
                     </li>
