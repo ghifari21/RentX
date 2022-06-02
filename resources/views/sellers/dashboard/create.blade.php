1c2a13f0bfcc49f35ab990835545fbf9e099ded3
Hilman Fauzi Herdiana <kk726627@gmail.com>
2022-06-02 21:42:41 +0700

    changes navbar on sellers

--- resources/views/sellers/dashboard/create.blade.php
+++ resources/views/sellers/dashboard/create.blade.php
@@ -1,14 +1,11 @@
 @extends('layouts.main')
 
 @section('main-content')
-    <!-- navbar section -->
-    @include('partials.navbar2')
+<!-- navbar section -->
+@include('partials.navbar2')
 
-    <div class="container px-5 mb-5">
-        <h4 class="fw-bold">Tambah Kos / Kontrakan</h4>
-
-        <!-- ================================
-              vvvv  MASIH BELUM BERES   vvvv  -->
+<div class="container px-5 mb-5">
+    <h4 class="fw-bold">Tambah Kos / Kontrakan</h4>
     <form action="/seller/dashboard" method="POST" enctype="multipart/form-data">
         @csrf
         <div class="add-rentplace border border-3 test rounded p-3 mt-4">
@@ -179,66 +176,66 @@
 
         <div class="add-rentplace border border-3 rounded p-3 mt-4">
             <p class="add-rentplace-label fw-bold">Alamat Kos / Kontrakan</p>
-                <div class="form-group row mb-2">
-                    <label for="inputAlamat" class="col-sm-2 col-form-label add-rentplace-label">Alamat</label>
-                    <div class="col-sm-10">
-                        <textarea rows="4" class="form-control @error('address') is-invalid @enderror" id="address" name="address" placeholder="Alamat dari Kos / Kontrakan">{{ old('address') }}</textarea>
-                        @error('address')
-                            <div class="invalid-feedback">
-                                {{ $message }}
-                            </div>
-                        @enderror
-                    </div>
+            <div class="form-group row mb-2">
+                <label for="inputAlamat" class="col-sm-2 col-form-label add-rentplace-label">Alamat</label>
+                <div class="col-sm-10">
+                    <textarea rows="4" class="form-control @error('address') is-invalid @enderror" id="address" name="address" placeholder="Alamat dari Kos / Kontrakan">{{ old('address') }}</textarea>
+                    @error('address')
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                    @enderror
+                </div>
+            </div>
+
+            <div class="form-group row mb-2">
+                <label for="" class="col-sm-2 col-form-label add-rentplace-label"></label>
+                <div class="col-sm">
+                    <label class="add-rentplace-label" for="inputProvinsi">Provinsi</label>
+                    <input type="text" class="form-control @error('province') is-invalid @enderror" name="province" id="inputProvinsi" placeholder="Provinsi" value="{{ old('province') }}">
+                    @error('province')
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                    @enderror
                 </div>
 
-                <div class="form-group row mb-2">
-                    <label for="" class="col-sm-2 col-form-label add-rentplace-label"></label>
-                    <div class="col-sm">
-                        <label class="add-rentplace-label" for="inputProvinsi">Provinsi</label>
-                        <input type="text" class="form-control @error('province') is-invalid @enderror" name="province" id="inputProvinsi" placeholder="Provinsi" value="{{ old('province') }}">
-                        @error('province')
-                            <div class="invalid-feedback">
-                                {{ $message }}
-                            </div>
-                        @enderror
-                    </div>
-
-                    <div class="col-sm">
-                        <label class="add-rentplace-label" for="inputKabupatenKota">Kabupaten/Kota</label>
-                        <input type="text" class="form-control @error('city') is-invalid @enderror" name="city" id="inputKabupatenKota" placeholder="Kabupaten/Kota" value="{{ old('city') }}">
-                        @error('city')
-                            <div class="invalid-feedback">
-                                {{ $message }}
-                            </div>
-                        @enderror
-                    </div>
-
-                    <div class="col-sm">
-                        <label class="add-rentplace-label" for="inputKecamatan">Kecamatan</label>
-                        <input type="text" class="form-control @error('district') is-invalid @enderror" name="district" id="inputKecamatan" placeholder="Kecamatan" value="{{ old('district') }}">
-                        @error('district')
-                            <div class="invalid-feedback">
-                                {{ $message }}
-                            </div>
-                        @enderror
-                    </div>
+                <div class="col-sm">
+                    <label class="add-rentplace-label" for="inputKabupatenKota">Kabupaten/Kota</label>
+                    <input type="text" class="form-control @error('city') is-invalid @enderror" name="city" id="inputKabupatenKota" placeholder="Kabupaten/Kota" value="{{ old('city') }}">
+                    @error('city')
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                    @enderror
                 </div>
 
-                <div class="form-group row mb-2">
-                    <label for="inputLink" class="col-sm-2 col-form-label add-rentplace-label">Link Maps</label>
-                    <div class="col-sm-10">
-                        <input type="url" class="form-control" id="inputLink" name="link_location" placeholder="URL Lokasi Kos / Kontrakan" value="{{ old('link_location') }}">
-                    </div>
+                <div class="col-sm">
+                    <label class="add-rentplace-label" for="inputKecamatan">Kecamatan</label>
+                    <input type="text" class="form-control @error('district') is-invalid @enderror" name="district" id="inputKecamatan" placeholder="Kecamatan" value="{{ old('district') }}">
+                    @error('district')
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                    @enderror
                 </div>
+            </div>
+
+            <div class="form-group row mb-2">
+                <label for="inputLink" class="col-sm-2 col-form-label add-rentplace-label">Link Maps</label>
+                <div class="col-sm-10">
+                    <input type="url" class="form-control" id="inputLink" name="link_location" placeholder="URL Lokasi Kos / Kontrakan" value="{{ old('link_location') }}">
+                </div>
+            </div>
         </div>
 
         <div class="add-rentplace-buttons d-flex justify-content-end mt-4">
             <button class="btn btn-wide btn-danger cancel me-3" type="cancel">Batal</button>
             <button class="btn btn-wide btn-success save" type="submit">Simpan</button>
         </div>
-    </div>
-</form>
+    </form>
+</div>
 
-    <!-- footer section  -->
-    @include('partials.footer2')
+<!-- footer section  -->
+@include('partials.footer2')
 @endsection
